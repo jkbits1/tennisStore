bfa3cf6c454eacb6d4a17abb2456b1b4354d75ac
jkbits1 <jkbits1@users.noreply.github.com>
2014-11-13 02:04:12 +0000

    support for cors

--- expressBasic.js
+++ expressBasic.js
@@ -5,6 +5,7 @@
 "use strict";
 
 var express = require('express');
+var cors = require('cors');
 
 var filesInfo = require('./tennisDb');
 
@@ -14,12 +15,43 @@ console.log(JSON.stringify(filesInfo.getFilesInfo));
 
 var app = express();
 
+// NOTE: currently not using this middleware due to
+// app.all() fix below. However, it may come in useful.
+//app.use(cors());
+
 // cors stuff
-app.all('/', function (req, res, next) {
+// NOTE: as POST request comes to path '/note', these
+//       headers were not being sent before  changed
+//       '/' to '*'
+//app.all('/', function (req, res, next) {
+app.all('*', function (req, res, next) {
+
+  // trying to reverse-engineer cors fix.
+  // There's more of this before next(), below.
+  if (req.method === 'OPTIONS') {
+
+    console.error('OPTIONS method');
+  }
+
 
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
+//  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  res.header("Access-Control-Allow-Credentials", true);
+
+  if (req.method === 'OPTIONS') {
+
+    // cribbed from cors() middleware - see above.
+    res.statusCode = 204;
+    res.end();
+
+    console.error('OPTIONS method');
+  }
+  else {
+
+    next();
+  }
 });
 
 app.get('/', function (req, res) {
@@ -32,6 +64,19 @@ app.get('/', function (req, res) {
   });
 
 //  res.send('hi');
+});
+
+app.post('/note', function (req, res) {
+
+  req.pipe(process.stderr);
+
+
+  console.log("note from client");
+
+//  res.header("Access-Control-Allow-Origin", "*");
+//  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+  res.send("got a note");
 });
 
 app.listen(3000);
