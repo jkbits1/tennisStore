463b4cefa0b8072b62503af60d9719dd095865bf
jkbits1 <jkbits1@users.noreply.github.com>
2014-12-27 02:00:15 +0000

    basic info server

---
+++ expressVideos.js
@@ -0,0 +1,21 @@
+/**
+ * Created by jk on 27/12/14.
+ */
+
+var express = require('express');
+var getFolderList = require('./fileParse');
+
+var app = express();
+
+app.get('/', function (req, res) {
+
+  getFolderList("H:\\Video\\SG3\\TVee\\SGATE AT", function(err, list) {
+
+    res.send({
+      files: list
+    });
+  });
+
+});
+
+var server = app.listen(3030, function() {});
