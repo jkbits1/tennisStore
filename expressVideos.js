c84411b5f0311aa40f58d0fddaad71be0b277e6d
jkbits1 <jkbits1@users.noreply.github.com>
2015-01-05 22:55:08 +0000

    very basic cors support

--- expressVideos.js
+++ expressVideos.js
@@ -7,6 +7,13 @@ var getFolderList = require('./fileParse');
 
 var app = express();
 
+app.all('*', function (req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+
+  next();
+});
+
 app.get('/', function (req, res) {
 
   getFolderList("H:\\Video\\SG3\\TVee\\SGATE AT", function(err, list) {
