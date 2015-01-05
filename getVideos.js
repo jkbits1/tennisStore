f20f4ea72d1240dc67db4fabb0abf9a4b3fd3de2
jkbits1 <jkbits1@users.noreply.github.com>
2015-01-05 22:56:32 +0000

    uses fileParse module to display programme info

--- getVideos.js
+++ getVideos.js
@@ -2,4 +2,13 @@
  * Created by jk on 26/12/14.
  */
 
+var getFolderList = require('./fileParse');
 
+getFolderList("H:\\Video\\SG3\\TVee\\SGATE AT", function (err, list) {
+
+  list.forEach(function (file) {
+
+    //console.log("file:", file);
+    console.log("Prog:", file.fileName, file.date.toDateString(), file.time);
+  });
+});
