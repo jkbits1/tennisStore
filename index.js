e0bf23510d11c82b27f79f7ca497e1c97774a021
jkbits1 <jkbits1@users.noreply.github.com>
2015-03-19 03:28:25 +0000

    env vars

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
+  var result = '';
+  var times = process.env.TIMES || 5;
+
+  for (i=0; i<times; i++) {
+    result += cool();
+  }
   //response.send('Hello World!');
-  response.send(cool());
+  //response.send(cool());
+  response.send(result);
 });
 
 app.listen(app.get('port'), function() {
