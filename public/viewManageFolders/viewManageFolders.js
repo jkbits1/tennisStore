8420ef23feb81be7ee5756c98685a45d750d6939
jkbits1 <jkbits1@users.noreply.github.com>
2015-03-24 21:58:08 +0000

    added angular viewManageFolders

---
+++ public/viewManageFolders/viewManageFolders.js
@@ -0,0 +1,43 @@
+/**
+ * Created by jk on 17/03/15.
+ */
+
+'use strict';
+
+var manageModule = angular.module('myApp.viewManageFolders', ['ngRoute']);
+
+manageModule.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.when('/manageFolders', {
+    templateUrl: function (params){
+      return 'viewManageFolders/viewManageFolders.html';
+    },
+    controller: 'manageFoldersCtrl'
+  })
+}]);
+
+manageModule.controller('manageFoldersCtrl', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http){
+
+  $scope.folders = []; //[{name: "f1"}, {name: "f2"}];
+
+  $scope.folderInfo = {
+    id: '',
+    name: '',
+    path: ''
+  };
+
+  $http.get('http://localhost:3030/foldersDb')
+    .success(function (data, status, headers, config) {
+      $scope.folders = data;
+    })
+    .error(function (data, status, headers, config) {
+
+    });
+
+  $scope.addFolder = function (folderInfo) {
+    $http.post('http://localhost:3030/addPath', folderInfo)
+      .success(function (data, status, headers, config) {
+
+    })
+      .error(function (data, status, headers, config){});
+  };
+}]);
