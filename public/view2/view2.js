5e8a30070ff0176d2bf2a41eda0c976e0e2a1fe5
jkbits1 <jkbits1@users.noreply.github.com>
2015-04-11 22:31:24 +0100

    removed filenameService and added fn to return summary item

--- public/view2/view2.js
+++ public/view2/view2.js
@@ -19,93 +19,6 @@ var view2Module = angular.module('myApp.view2', ['ngRoute'])
   ;
 }]);
 
-view2Module.factory('filenameService', function () {
-  var t1 = "";
-
-  return {
-
-    // NOTE: although both methods work, not sure whether to use this.fn or pass in filenameService as an arg and use filenameService.fn
-    setUpScope: function($scope) {
-
-      $scope.getDateAsString = this.getDateAsString;
-      $scope.getProgramName = this.getProgramName($scope);
-
-      $scope.getFirstEpisodeDate = this.getFirstEpisodeDate
-        //($scope)
-      ;
-      $scope.getLastEpisodeDate = this.getLastEpisodeDate($scope);
-
-      $scope.getEpisodeCount = this.getEpisodeCount($scope);
-    },
-    getFileName: function (x) {
-      return "123";
-    },
-    getDateAsString: function (date) {
-      var fileDate = new Date(date);
-      return fileDate.toDateString();
-    },
-    getProgramName: function ($scope) {
-
-      return function() {
-        if ($scope.files === undefined || $scope.files.length === 0) {
-          return "";
-        }
-
-        return $scope.files[0].fileName;
-      };
-    },
-
-    // NOTE: both patterns below work equally well (for all service fns), i) using a closure to allow use of scope, and ii) simply using this, relying on that being scope. I prefer the closure as more readable, possibly future-proof but is perhaps slightly less efficient.
-    //getFirstEpisodeDate: function($scope) {
-    //
-    //  return function() {
-    //
-    //    if ($scope.files === undefined) {
-    //
-    //      return "";
-    //    }
-    //
-    //    return new Date($scope.files[0].date).toDateString();
-    //  }
-    //},
-    getFirstEpisodeDate: function() {
-
-        //if ($scope.files === undefined) {
-        if (this.files === undefined || this.files.length === 0) {
-          return "";
-        }
-
-        return new Date(this.files[0].date).toDateString();
-    },
-
-    getLastEpisodeDate: function($scope) {
-
-      return function() {
-        if ($scope.files === undefined || $scope.files.length === 0) {
-
-          return "";
-        }
-
-        var lastEpisodeIndex = $scope.files.length - 1;
-
-        return new Date($scope.files[lastEpisodeIndex].date).toDateString();
-      }
-    },
-
-    getEpisodeCount: function($scope) {
-
-      return function() {
-        if ($scope.files === undefined || $scope.files.length === 0) {
-
-          return 0;
-        }
-
-        return $scope.files.length;
-      }
-    }
-  };
-});
-
 view2Module.controller('View2Ctrl', ['$rootScope', '$scope', '$http', '$routeParams', 'filenameService', function($rootScope, $scope, $http, $routeParams, filenameService) {
 
   var progIdUriSegment = "";
@@ -162,7 +75,7 @@ view2Module.controller('View2Ctrl', ['$rootScope', '$scope', '$http', '$routePar
       //var parser = new fileParser($scope.files[0].fileName);
 
       $scope.fileName = $scope.getProgramName();
-      //$scope.fileDate = $scope.files[0].date;
+      $scope.summary = $scope.getProgrammeSummary();
 
   }).error(function(data, status, headers, config) {
 
@@ -171,28 +84,28 @@ view2Module.controller('View2Ctrl', ['$rootScope', '$scope', '$http', '$routePar
   filenameService.setUpScope($scope);
 }]);
 
-function fileParser(file) {
-
-  this.getDate = function(){
-
-    var regexFileName = /[a-zA-Z\s]*/;
-
-    var fileDate = getFileInfo(file, regexFileName);
-  }
-
-  function getFileInfo(fileName, regex) {
-
-    var items = fileName.match(regex);
-    var itemInfo = undefined;
-
-    items.forEach(function (item) {
-
-      //console.log("matches:", item);
-
-      itemInfo = item;
-    });
-
-    return itemInfo;
-  }
-}
+//function fileParser(file) {
+//
+//  this.getDate = function(){
+//
+//    var regexFileName = /[a-zA-Z\s]*/;
+//
+//    var fileDate = getFileInfo(file, regexFileName);
+//  }
+//
+//  function getFileInfo(fileName, regex) {
+//
+//    var items = fileName.match(regex);
+//    var itemInfo = undefined;
+//
+//    items.forEach(function (item) {
+//
+//      //console.log("matches:", item);
+//
+//      itemInfo = item;
+//    });
+//
+//    return itemInfo;
+//  }
+//}
 
